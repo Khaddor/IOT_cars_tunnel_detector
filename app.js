a50945e4e9aea94625b6fcba6cff6f913874c32d
aa195113 <abderrahmane.abaakil@etu.univ-lehavre.fr>
2024-02-14 20:02:26 +0100

    API Route for Command Sending

--- app.js
+++ app.js
@@ -1,12 +1,70 @@
 const express = require('express');
+const { SerialPort } = require('serialport');
+const bodyParser = require('body-parser');
+const { ReadlineParser } = require('@serialport/parser-readline');
+const cors = require('cors');
+const WebSocket = require('ws');
+const http = require('http');
+
 const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, Express!');
+app.use(cors());
+app.use(bodyParser.json());
+
+const ledPin1 = 2;
+const ledPin2 = 13;
+const detectorPin1 = 10;
+const detectorPin2 = 11;
+let lastState1 = true;
+let lastState2 = true;
+let carsInTunnel = 0;
+
+const serialPort = new SerialPort({ path: '/dev/ttyACM0', baudRate: 9600 });
+const parser = serialPort.pipe(new ReadlineParser({ delimiter: '\n' }));
+
+serialPort.on('open', () => {
+  console.log('Serial port is open');
 });
 
-// Start the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+
+wss.on('connection', (ws) => {
+    ws.send(JSON.stringify({ carsInTunnel }));
+  });
+
+  app.post('/api/close_tunnel', (req, res) => {
+    if (req.body.command === 'ON') {
+      serialPort.write('LED2_ON\n', function(err) {
+        if (err) {
+          return console.log('Error writing to serial port:', err.message);
+        }
+        console.log('Command sent to Arduino: LED2_ON');
+      });
+    } else {
+      serialPort.write('LED2_OFF\n', function(err) {
+        if (err) {
+          return console.log('Error writing to serial port:', err.message);
+        }
+        console.log('Command sent to Arduino: LED2_OFF');
+      });
+    }
+    res.json({ status: 'success' });
+  });
+  
+
+parser.on('data', (data) => {
+  console.log('Data from Arduino:', data);
+  carsInTunnel = parseInt(data.trim());
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify({ carsInTunnel }));
+    }
+  });
 });
+
+server.listen(port, () => {
+    console.log(`API Server is running on http://localhost:${port}`);
+  });
